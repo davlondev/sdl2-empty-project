1de88d41cd0799b70ace1b72d4fb369f95cbe39b
candyslug <david.wip.long@gmail.com>
2022-09-23 23:22:23 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+int main() {
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello, World!", 
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 800, NULL
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED
+    );
+
+    SDL_Color red = (SDL_Color){255, 0, 0};
+    SDL_Rect rectangle_demo = (SDL_Rect){50, 50, 300, 300};
+
+    bool run = true;
+    while (run) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_WINDOWEVENT:
+                if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
+                    run = false;
+                    break;
+                }
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    run = false;
+                    break;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderFillRect(renderer, &rectangle_demo);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    return 0;
+}
